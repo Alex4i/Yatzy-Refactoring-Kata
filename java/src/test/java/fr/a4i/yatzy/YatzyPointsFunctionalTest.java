d2bec20b1c87cc4a0d52c75fd477c1d51589a76d
Alexandre Catry <alexandre.catry@ratp.fr>
2024-01-14 19:28:59 +0100

    test parameterization

--- java/src/test/java/fr/a4i/yatzy/YatzyPointsFunctionalTest.java
+++ java/src/test/java/fr/a4i/yatzy/YatzyPointsFunctionalTest.java
@@ -1,108 +1,92 @@
 package fr.a4i.yatzy;
 
 import fr.a4i.yatzy.model.DiceThrow;
+import lombok.Value;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.params.ParameterizedTest;
+
+
 class YatzyPointsFunctionalTest {
 
-    @Test
-    void onesTest(){
-        assertThat(YatzyPoints.ones(new DiceThrow(2,3,4,5,6))).isZero();
-        assertThat(YatzyPoints.ones(new DiceThrow(1,1,2,3,4))).isEqualTo(2);
-    }
+    private static Iterable<Case> cases() {
+        return List.of(
+            new Case("ones1",YatzyPoints::ones, new DiceThrow(2, 3, 4, 5, 6), 0),
+            new Case("ones2", YatzyPoints::ones,new DiceThrow(1, 1, 2, 3, 4), 2),
 
-    @Test
-    void twosTest(){
-        assertThat(YatzyPoints.twos(new DiceThrow(1,3,4,5,6))).isZero();
-        assertThat(YatzyPoints.twos(new DiceThrow(2,2,2,3,4))).isEqualTo(6);
-    }
+            new Case("twos1", YatzyPoints::twos,new DiceThrow(1,3,4,5,6), 0),
+            new Case("twos2", YatzyPoints::twos,new DiceThrow(2,2,2,3,4), 6),
 
-    @Test
-    void threesTest(){
-        assertThat(YatzyPoints.threes(new DiceThrow(1,2,4,5,6))).isZero();
-        assertThat(YatzyPoints.threes(new DiceThrow(3,3,3,3,4))).isEqualTo(12);
-    }
+            new Case("threes1", YatzyPoints::threes,new DiceThrow(1,2,4,5,6), 0),
+            new Case("threes2", YatzyPoints::threes,new DiceThrow(3,3,3,3,4), 12),
 
-    @Test
-    void foursTest(){
-        assertThat(YatzyPoints.fours(new DiceThrow(1,2,3,5,6))).isZero();
-        assertThat(YatzyPoints.fours(new DiceThrow(3,3,3,4,4))).isEqualTo(8);
-    }
+            new Case("fours1", YatzyPoints::fours,new DiceThrow(1,2,3,5,6), 0),
+            new Case("fours2", YatzyPoints::fours,new DiceThrow(3,3,3,4,4), 8),
 
-    @Test
-    void fivesTest(){
-        assertThat(YatzyPoints.fives(new DiceThrow(1,2,3,4,6))).isZero();
-        assertThat(YatzyPoints.fives(new DiceThrow(3,3,5,5,5))).isEqualTo(15);
-    }
+            new Case("fives1", YatzyPoints::fives,new DiceThrow(1,2,3,4,6), 0),
+            new Case("fives2", YatzyPoints::fives,new DiceThrow(3,3,5,5,5), 15),
 
-    @Test
-    void sixesTest(){
-        assertThat(YatzyPoints.sixes(new DiceThrow(1,2,3,4,2))).isZero();
-        assertThat(YatzyPoints.sixes(new DiceThrow(6,6,3,6,6))).isEqualTo(24);
-    }
+            new Case("sixes1", YatzyPoints::sixes,new DiceThrow(1,2,3,4,2), 0),
+            new Case("sixes2", YatzyPoints::sixes,new DiceThrow(6,6,3,6,6), 24),
 
-    @Test
-    void onePairTest(){
-        assertThat(YatzyPoints.onePair(new DiceThrow(1,2,3,4,5))).isZero();
-        assertThat(YatzyPoints.onePair(new DiceThrow(3,6,6,6,6))).isEqualTo(12);
-        assertThat(YatzyPoints.onePair(new DiceThrow(3,4,3,5,6,4))).isEqualTo(8);
-    }
+            new Case("onePair1", YatzyPoints::onePair,new DiceThrow(1,2,3,4,5), 0),
+            new Case("onePair2", YatzyPoints::onePair,new DiceThrow(3,6,6,6,6), 12),
+            new Case("onePair3", YatzyPoints::onePair,new DiceThrow(3,4,3,5,4), 8),
 
-    @Test
-    void twoPairsTest(){
-        assertThat(YatzyPoints.twoPairs(new DiceThrow(1,2,3,4,5))).isZero();
-        assertThat(YatzyPoints.twoPairs(new DiceThrow(6,6,3,6,6))).isZero();
-        assertThat(YatzyPoints.twoPairs(new DiceThrow(3,4,4,5,5))).isEqualTo(18);
-    }
+            new Case("twoPairs1", YatzyPoints::twoPairs,new DiceThrow(1,2,3,4,5), 0),
+            new Case("twoPairs2", YatzyPoints::twoPairs,new DiceThrow(6,6,3,6,6), 0),
+            new Case("twoPairs3", YatzyPoints::twoPairs,new DiceThrow(3,4,4,5,5), 18),
 
-    @Test
-    void threeOfAKindTest(){
-        assertThat(YatzyPoints.threeOfAKind(new DiceThrow(1,2,3,4,5))).isZero();
-        assertThat(YatzyPoints.threeOfAKind(new DiceThrow(2,2,3,3,6))).isZero();
-        assertThat(YatzyPoints.threeOfAKind(new DiceThrow(3,3,3,5,5))).isEqualTo(9);
-    }
+            new Case("threeOfAKind1", YatzyPoints::threeOfAKind,new DiceThrow(1,2,3,4,5), 0),
+            new Case("threeOfAKind2", YatzyPoints::threeOfAKind,new DiceThrow(2,2,3,3,6), 0),
+            new Case("threeOfAKind3", YatzyPoints::threeOfAKind,new DiceThrow(3,3,3,5,5), 9),
 
-    @Test
-    void fourOfAKindTest(){
-        assertThat(YatzyPoints.fourOfAKind(new DiceThrow(1,2,3,4,5))).isZero();
-        assertThat(YatzyPoints.fourOfAKind(new DiceThrow(3,5,5,5,5))).isEqualTo(20);
-    }
+            new Case("fourOfAKind1", YatzyPoints::fourOfAKind,new DiceThrow(1,2,3,4,5), 0),
+            new Case("fourOfAKind2", YatzyPoints::fourOfAKind,new DiceThrow(3,5,5,5,5), 20),
 
-    @Test
-    void smallStraightTest(){
-        assertThat(YatzyPoints.smallStraight(new DiceThrow(3,5,5,5,5))).isZero();
-        assertThat(YatzyPoints.smallStraight(new DiceThrow(1,2,3,4,5))).isEqualTo(15);
-    }
+            new Case("smallStraight1", YatzyPoints::smallStraight,new DiceThrow(3,5,5,5,5), 0),
+            new Case("smallStraight1", YatzyPoints::smallStraight,new DiceThrow(1,2,3,4,5), 15),
 
-    @Test
-    void largeStraightTest(){
-        assertThat(YatzyPoints.largeStraight(new DiceThrow(3,5,5,5,5))).isZero();
-        assertThat(YatzyPoints.largeStraight(new DiceThrow(2,3,4,5,6))).isEqualTo(20);
-    }
+            new Case("largeStraight1", YatzyPoints::largeStraight,new DiceThrow(3,5,5,5,5), 0),
+            new Case("largeStraight2", YatzyPoints::largeStraight,new DiceThrow(2,3,4,5,6), 20),
 
-    @Test
-    void fullHouseTest(){
-        assertThat(YatzyPoints.fullHouse(new DiceThrow(3,5,5,5,5))).isZero();
-        assertThat(YatzyPoints.fullHouse(new DiceThrow(1,1,1,2,2))).isEqualTo(7);
-        assertThat(YatzyPoints.fullHouse(new DiceThrow(3,3,5,5,5))).isEqualTo(21);
-    }
+            new Case("fullHouse1", YatzyPoints::fullHouse,new DiceThrow(3,5,5,5,5), 0),
+            new Case("fullHouse2", YatzyPoints::fullHouse,new DiceThrow(1,1,1,2,2), 7),
+            new Case("fullHouse3", YatzyPoints::fullHouse,new DiceThrow(3,3,5,5,5), 21),
 
-    @Test
-    void chanceTest(){
-        assertThat(YatzyPoints.chance(new DiceThrow(3,5,5,5,5))).isEqualTo(23);
-        assertThat(YatzyPoints.chance(new DiceThrow(1,2,3,4,5))).isEqualTo(15);
-        assertThat(YatzyPoints.chance(new DiceThrow(3,3,5,5,5))).isEqualTo(21);
+            new Case("chance1", YatzyPoints::chance,new DiceThrow(3,5,5,5,5), 23),
+            new Case("chance2", YatzyPoints::chance,new DiceThrow(1,2,3,4,5), 15),
+            new Case("chance2", YatzyPoints::chance,new DiceThrow(3,3,5,5,5), 21),
+
+            new Case("yatzy1", YatzyPoints::yatzy,new DiceThrow(3,5,5,5,5), 0),
+            new Case("yatzy2", YatzyPoints::yatzy,new DiceThrow(1,1,1,1,1), 50),
+            new Case("yatzy3", YatzyPoints::yatzy,new DiceThrow(5,5,5,5,5), 50)
+        );
+    };
+
+    @ParameterizedTest
+    @MethodSource("cases")
+    void test(Case aCase){
+        assertThat(aCase.countingMethod.apply(aCase.input)).isEqualTo(aCase.result);
     }
 
 
-    @Test
-    void yatzyTest(){
-        assertThat(YatzyPoints.yatzy(new DiceThrow(3,5,5,5,5))).isZero();
-        assertThat(YatzyPoints.yatzy(new DiceThrow(1,1,1,1,1))).isEqualTo(50);
-        assertThat(YatzyPoints.yatzy(new DiceThrow(5,5,5,5,5))).isEqualTo(50);
+    @Value
+    private static class Case {
+        String name;
+        Function<DiceThrow,Integer> countingMethod;
+        DiceThrow input;
+        Integer result;
+
+        @Override
+        public String toString() {
+            return name;
+        }
     }
-
-
 }
